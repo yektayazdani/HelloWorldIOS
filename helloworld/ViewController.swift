a1a4ac193c8b8bd96d0a8b93e6a43ef28c256802
yekta.yazdani <yekta.yazdani@sikkasoftware.com>
2018-02-28 11:27:45 -0800

    Initial Commit

---
+++ helloworld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  helloworld
+//
+//  Created by Yekta Yazdani on 2/28/18.
+//  Copyright © 2018 Yekta Yazdani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
