a800f6dd64f777b85f7800ac1f4acb9f3b8fc61f
Yekta Yazdani <yekta.yazdani@sikkasoftware.com>
2018-02-28 12:09:04 -0800

    first commit

--- helloworld/ViewController.swift
+++ helloworld/ViewController.swift
@@ -9,7 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var NameLabel: UILabel!
+    @IBOutlet var atextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBOutlet var helloworldaction: UIButton!
+    @IBAction func onHit(_ sender: Any) {
+        NameLabel.text = "Hello \(atextField.text!)"
+    }
+    
 }
 
